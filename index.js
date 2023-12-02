71822fa5392e766f6cf275aac9176a7da115376f
Torsten Paul <Torsten.Paul@gmx.de>
2023-12-03 00:49:28 +0100

    Update info text.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ setDay(3, { author: 'Torsten Paul', license: 'CC0', dir: 'Lamp-Part1',
         img: 'Lamp-christmas_Side1.png',
         src: ['Lamp-Part1.scad', 'christmas/gingerbread.svg', 'disco/Disco-Dancer-3-Remix-by-Merlin2525.svg', 'cartoon/Cartoon dog.svg', 'flower/Flower21.svg', 'winter/SnowFlake02.svg'],
         bin: 'Lamp-christmas_Side1.3mf',
-	info: 'This is part 1 of 6 of a printable lamp, but the panels could be also used separately. There are 5 themes that can be used with the SCAD file, the attached print files is for the christmas theme lamp. All the panel 3MF files are prepared for printing 2 layers @ 0.2mm in white and then in some other color. Many thanks to all the people who have shared those SVG images as public domain, see the SCAD script for links.'
+	info: '<p>This is part 1 of 6 of a printable lamp, but the panels could be also used separately. There are SVG files for 5 themes that can be used by placing them into the theme folder along with the SCAD file.</p><p>The attached print files is for the christmas theme lamp. All the panel 3MF files are prepared for printing 2 layers @ 0.2mm in white and then in some other color.</p><p>Many thanks to all the people who have shared those SVG images as public domain, see the SCAD script for links.</p>'
 });
 setDay(4, { author: 'Torsten Paul', license: 'CC0', dir: 'Tree',
         img: 'Tree.png',
